5d14fe774305ca1335b0e1a4154654fa4dfedaf7
Sebastian <sebastianmostert663@gmail.com>
2024-07-25 08:59:08 +0200

    Update layout.js
    Simplify to debug

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,39 +1,27 @@
-import { ThemeContextProvider } from '@/context/ThemeContext';
-import Footer from '@/components/footer/Footer';
-import Navbar from '@/components/navbar/Navbar';
-import './globals.css';
-import { Inter } from 'next/font/google';
-import ThemeProvider from '@/providers/ThemeProvider';
-import AuthProvider from '@/providers/AuthProvider';
-import React, { Suspense } from 'react';
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'LLIS - Student News',
   description: 'The official LLIS student news site',
-};
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <ThemeContextProvider>
-            <ThemeProvider>
-              <div className="container">
-                <div className="wrapper">
-                  <Suspense fallback={<div>Loading navbar...</div>}>
-                    <Navbar />
-                  </Suspense>
-                  {children}
-                  <Footer />
-                </div>
-              </div>
-            </ThemeProvider>
-          </ThemeContextProvider>
-        </AuthProvider>
+        <div className="container">
+          <div className="wrapper">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>
       </body>
     </html>
-  );
+  )
 }
