0a0b1c827da0212854e59df94c1cac06324345c7
Sebastian <sebastianmostert663@gmail.com>
2024-07-25 09:01:07 +0200

    Update layout.js
    Added suspense

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,39 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/footer/Footer'
+import React, { Suspense } from 'react';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/navbar/Navbar';
+import Footer from '@/components/footer/Footer';
+import { ThemeContextProvider } from '@/context/ThemeContext';
+import ThemeProvider from '@/providers/ThemeProvider';
+import AuthProvider from '@/providers/AuthProvider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'LLIS - Student News',
   description: 'The official LLIS student news site',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container">
-          <div className="wrapper">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </div>
+        <AuthProvider>
+          <ThemeContextProvider>
+            <ThemeProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Suspense fallback={<div>Loading navbar...</div>}>
+                    <Navbar />
+                  </Suspense>
+                  {children}
+                  <Footer />
+                </div>
+              </div>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
