95cf3ad1a4cf6ed64216bf38ba8ca0ca39066d3c
Sebastian <sebastianmostert663@gmail.com>
2024-07-26 17:51:32 +0200

    Update page.jsx

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,9 +1,10 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
-import styles from './adminPage.module.css'
 import { useRouter } from 'next/navigation';
 import { FaComment, FaNewspaper, FaUsers } from 'react-icons/fa';
+import { Box, Typography, Grid, Card, CardContent, CardMedia, Skeleton } from '@mui/material';
+import styles from './adminPage.module.css';
 
 const getData = async () => {
     const res = await fetch('/api/user/', {
@@ -36,6 +37,102 @@ const cardData = [
     },
 ];
 
+const AdminCard = ({ card, router }) => {
+    return (
+        <Grid item xs={12} sm={6} md={4}>
+            <Card
+                onClick={() => router.push(card.url)}
+                sx={{
+                    backgroundColor: 'var(--softBg)',
+                    color: 'var(--textColor)',
+                    textAlign: 'center',
+                    borderRadius: '8px',
+                    boxShadow: 3,
+                    cursor: 'pointer',
+                    margin: 'auto',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '100%', // Ensure the card stretches to fill the container height
+                    '&:hover': {
+                        transform: 'scale(1.05)',
+                    },
+                    transition: 'transform 0.3s',
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        padding: 2,
+                    }}
+                >
+                    {card.icon}
+                </Box>
+                <CardContent
+                    sx={{
+                        padding: 2,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        flexGrow: 1, // Allow content to expand
+                    }}
+                >
+                    <Typography variant="h6" component="h3">
+                        {card.title}
+                    </Typography>
+                    <Typography variant="body2">
+                        {card.description}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
+    );
+};
+
+const SkeletonCard = () => {
+    return (
+        <Grid item xs={12} sm={6} md={4}>
+            <Card sx={{
+                backgroundColor: 'var(--softBg)',
+                color: 'var(--textColor)',
+                textAlign: 'center',
+                borderRadius: '8px',
+                boxShadow: 3,
+                cursor: 'pointer',
+                margin: 'auto',
+                transition: 'transform 0.3s',
+                height: '100%', // Ensure the card stretches to fill the container height
+                '&:hover': {
+                    transform: 'scale(1.05)',
+                },
+            }}>
+                <Box sx={{
+                    height: 140,
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    margin: 'auto',
+                }}>
+                    <Skeleton variant="circular" height={80} width={80} />
+                </Box>
+                <CardContent sx={{
+                    paddingTop: 0, // Remove extra padding from the top
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                    <Skeleton variant="text" height={30} width="80%" />
+                    <Skeleton variant="text" height={20} width="60%" />
+                </CardContent>
+            </Card>
+        </Grid>
+    );
+};
+
+
 const AdminPage = () => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -51,7 +148,21 @@ const AdminPage = () => {
     }, [router]);
 
     if (loading) {
-        return <div>Loading...</div>;
+        return (
+            <Box sx={{ backgroundColor: 'var(--bg)', color: 'var(--textColor)', padding: 4 }}>
+                <Typography variant="h4" component="h1" sx={{ fontWeight: 'bold', textAlign: 'center', mb: 4 }}>
+                    <Skeleton variant="text" component={"h3"} width="25%" sx={{ marginLeft: 'auto', marginRight: 'auto' }} />
+                </Typography>
+                <Typography variant="h6" component="h2" sx={{ textAlign: 'center', mb: 4 }}>
+                    <Skeleton variant="text" component={"h5"} width="20%" sx={{ marginLeft: 'auto', marginRight: 'auto' }} />
+                </Typography>
+                <Grid container spacing={4}>
+                    {cardData.map((_, index) => (
+                        <SkeletonCard key={index} />
+                    ))}
+                </Grid>
+            </Box>
+        );
     }
 
     const isAdmin = user?.roles.includes("admin");
@@ -62,27 +173,19 @@ const AdminPage = () => {
     }
 
     return (
-        <div className={styles.container}>
-            <div className={styles.header}>Admin Dashboard</div>
-            {isAdmin ? (
-                <div>
-                    <div className={styles.subheader}>Manage your site&apos;s content</div>
-                    <div className={styles.cardContainer}>
-                        {cardData.map((card, index) => (
-                            <div key={index} className={styles.card} onClick={() => router.push(card.url)}>
-                                {card.icon}
-                                <h3>{card.title}</h3>
-                                <p>{card.description}</p>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            ) : (
-                <div>
-                    If you are seeing this, something has gone terribly wrong. Please contact the developer immediately and explain your situation.
-                </div>
-            )}
-        </div>
+        <Box sx={{ backgroundColor: 'var(--bg)', color: 'var(--textColor)', padding: 4 }}>
+            <Typography variant="h4" component="h1" sx={{ fontWeight: 'bold', textAlign: 'center', mb: 4 }}>
+                Admin Dashboard
+            </Typography>
+            <Typography variant="h6" component="h2" sx={{ textAlign: 'center', mb: 4 }}>
+                Manage your site&apos;s content
+            </Typography>
+            <Grid container spacing={4}>
+                {cardData.map((card, index) => (
+                    <AdminCard key={index} card={card} router={router} />
+                ))}
+            </Grid>
+        </Box>
     );
 };
 
